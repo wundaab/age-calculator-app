27005a75690de27dcfdde9c6560812775a16a304
wundaab <84353864+wundaab@users.noreply.github.com>
2023-05-07 14:17:56 +0000

    working js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-b_d = 9;
-b_m = 6;
-b_y = 2022;
+// //birth date
+// b_d = 22;
+// b_m = 8;
+// b_y = 2022;
 
-t_d = 7;
-t_m = 5;
-t_y = 2023;
+// //today's date
+// t_d = 7; 
+// t_m = 5;
+// t_y = 2023;
 
 var d=0, m=0, y=0;
+var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-y= t_y - b_y;
+y = t_y - b_y;
 
 if(b_m > t_m){
     y = y - 1;
@@ -20,10 +23,8 @@ else{
 
 if(b_d > t_d){
     m = m - 1;
-    d = 30 - (b_d - t_d);
+    d = month[t_m-1] - (b_d - t_d);
 }
 else{
     d = t_d - b_d;
 }
-
-console.log(y + " years " + m + " months " + d + " days");
